2388cd01ec24bec81664bc0229cfef8efe434ed9
Sharbi <seanharbison001@gmail.com>
2018-12-22 11:05:36 +0000

    Fix syntax

--- dcgan.py
+++ dcgan.py
@@ -10,6 +10,9 @@ import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import torch.optim as optim
 import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
