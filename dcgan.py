77d9797df0fefc75af9d31b5215cddae117a4324
Sharbi <seanharbison001@gmail.com>
2018-12-22 12:08:25 +0000

    Fix syntax

--- dcgan.py
+++ dcgan.py
@@ -43,7 +43,7 @@ dataset = dset.ImageFolder(root=dataroot,
                                transforms.Resize(image_size),
                                transforms.CenterCrop(image_size),
                                transforms.ToTensor(),
-                               transforms.Normalize((0.5), (0.5)),
+                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
 # Create the dataloader
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
