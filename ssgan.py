4044bf0b1752843b0018cb06641848cbb6033b21
Sharbi <seanharbison001@gmail.com>
2018-12-29 10:03:48 +0000

    Syntax fixes

--- ssgan.py
+++ ssgan.py
@@ -295,7 +295,7 @@ for epoch in range(num_epochs):
         output, d_class_logits_on_data, gan_logits_real, d_sample_features = netD(svhn_data)
 
         svhn_labels_one_hot = one_hot(svhn_labels)
-        d_class_loss_entropy = -torch.sum(svhn_labels_one_hot * torch.log(output), dim=1)
+        supervised_loss = d_criterion(svhn_labels, output)
 
         d_class_loss_entropy = d_class_loss_entropy.squeeze()
         delim = torch.max(torch.Tensor([1.0, torch.sum(label_mask.data)]))
