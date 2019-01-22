d03e96db9cbbec3bf53dd763537f4c4313de9c15
Sharbi <seanharbison001@gmail.com>
2019-01-22 18:49:35 +0000

    Syntax fixes

--- ssgan.py
+++ ssgan.py
@@ -169,28 +169,28 @@ class Discriminator(nn.Module):
             #nn.Dropout2d(0.5/2.5),
 
             # input is (number_channels) x 32 x 32
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(nc, (ndf, 4), 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
             # (ndf) x 16 x 16
-            nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d((ndf, 4), (ndf, 4), 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf),
             nn.LeakyReLU(0.2),
             # (ndf) x 8 x 8
-            nn.Conv2d(ndf, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d((ndf, 4), (ndf, 4), 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
             # (ndf) x 4 x 4
-            nn.Conv2d(ndf, ndf * 2, 3, 1, 1, bias=False),
+            nn.Conv2d((ndf, 4), (ndf * 2, 8), 3, 1, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2),
             # (ndf * 2) x 4 x 4
-            nn.Conv2d(ndf * 2, ndf * 2, 3, 1, 1, bias=False),
+            nn.Conv2d((ndf * 2, 8), (ndf * 2, 8), 3, 1, 1, bias=False),
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2),
             # (ndf * 2) x 4 x 4
-            nn.Conv2d(ndf * 2, ndf * 2, 3, 1, 0, bias=False),
+            nn.Conv2d((ndf * 2, 8), (ndf * 2, 8), 3, 1, 0, bias=False),
             nn.LeakyReLU(0.2),
             # (ndf * 2) x 2 x 2
         )
