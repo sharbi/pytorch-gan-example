e038191c8fe66a5bb7f70400472e64509dea427e
Sharbi <seanharbison001@gmail.com>
2019-01-29 01:36:38 +0000

    Fixed BatchNorm

--- ssgan.py
+++ ssgan.py
@@ -174,11 +174,11 @@ class Discriminator(nn.Module):
             nn.Dropout2d(0.5),
             # (ndf) x 30 x 2
             nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf),
+            nn.BatchNorm2d(ndf*2),
             nn.LeakyReLU(0.2),
             # (ndf) x 15 x 1
             nn.Conv2d(ndf * 2, ndf * 4, 4, (3, 1), 1, bias=False),
-            nn.BatchNorm2d(ndf),
+            nn.BatchNorm2d(ndf*4),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
             # (ndf) x 5 x 1
