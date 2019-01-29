ede9564a13241e0630825851320e35216e990c2a
Sharbi <seanharbison001@gmail.com>
2019-01-29 02:01:07 +0000

    Changed Discriminator kernel

--- ssgan.py
+++ ssgan.py
@@ -173,16 +173,16 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
             # (ndf) x 30 x 2
-            nn.Conv2d(ndf, ndf * 2, 4, 1, 1, bias=False),
+            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ndf*2),
             nn.LeakyReLU(0.2),
             # (ndf) x 15 x 1
-            nn.Conv2d(ndf * 2, ndf * 4, 3, 1, 1, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 2, 1, 1, bias=False),
             nn.BatchNorm2d(ndf*4),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
             # (ndf) x 5 x 1
-            nn.Conv2d(ndf * 4, ndf * 4, 3, 1, 1, bias=False),
+            nn.Conv2d(ndf * 4, ndf * 4, 2, 1, 1, bias=False),
             nn.LeakyReLU(0.2),
         )
 
