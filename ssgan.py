9e2cfd16f90e315c18db315b3261d2eb68dd35f1
Sharbi <seanharbison001@gmail.com>
2019-01-31 02:34:54 +0000

    Changed Discriminator size

--- ssgan.py
+++ ssgan.py
@@ -181,7 +181,7 @@ class Discriminator(nn.Module):
             nn.BatchNorm2d(ndf*2),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(0.5),
-            nn.Conv2d(ndf * 2, ndf * 4, 3, 1, 1, bias=False),
+            nn.Conv2d(ndf * 2, ndf * 4, 2, 1, 1, bias=False),
             nn.LeakyReLU(0.2),
         )
 
