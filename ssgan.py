4c78d3ca3a8c80ea1c3e498eadb82a22e8b34466
Sharbi <seanharbison001@gmail.com>
2019-02-04 14:38:49 +0000

    Added dimension expansion on data

--- ssgan.py
+++ ssgan.py
@@ -69,7 +69,7 @@ class DiabetesDataset(Dataset):
     def __getitem__(self, idx):
         data, label = self.diabetes_dataset.__getitem__(idx)
         data = np.expand_dims(data, axis=1)
-        data = self.transform(data)
+        data = _to_var(data)
         if self._is_train_dataset():
             return data, label, self.label_mask[idx]
         return data, label
