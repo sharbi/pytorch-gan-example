7ab4ee8c11dbd8c887230fa9da7b25a920b20451
Sharbi <seanharbison001@gmail.com>
2019-02-05 02:17:48 +0000

    Combine X and y values

--- ssgan.py
+++ ssgan.py
@@ -66,7 +66,7 @@ class DiabetesDataset(Dataset):
 
     def __getitem__(self, idx):
         data, labels = self.diabetes_dataset.__getitem__(idx)
-        data = np.expand_dims(data, axis=2)
+        data = np.expand_dims(data, axis=0)
         data = torch.FloatTensor(data)
         print(data.size())
         labels = torch.FloatTensor(labels)
