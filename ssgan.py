e873a289f2e738b758a811faefa31c31cee3f17c
Sharbi <seanharbison001@gmail.com>
2019-02-05 01:40:51 +0000

    Syntax fix

--- ssgan.py
+++ ssgan.py
@@ -84,8 +84,8 @@ def get_loader(batch_size):
     transform = transforms.Compose([
         transforms.ToTensor()])
 
-    diabetes_train = DiabetesDataset('../diabetes_data/', 'spline_X_processed.pkl', transform=transform, split='train')
-    diabetes_test = DiabetesDataset('../diabetes_data/', 'spline_X_processed.pkl', transform=transform, split='test')
+    diabetes_train = DiabetesDataset('../diabetes_data/', 'spline_X_processed.pkl', split='train')
+    diabetes_test = DiabetesDataset('../diabetes_data/', 'spline_X_processed.pkl', split='test')
 
     diabetes_loader_train = DataLoader(
         dataset=diabetes_train,
