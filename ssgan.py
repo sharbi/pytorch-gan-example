6cbdf1bcc366dcef5dbf1e3bcf9be11c5216cafc
Sharbi <seanharbison001@gmail.com>
2019-02-09 17:55:35 +0000

    Change to normalised dataset

--- ssgan.py
+++ ssgan.py
@@ -14,6 +14,7 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import numpy as np
+import pandas as pd
 
 import pickle as pkl
 
