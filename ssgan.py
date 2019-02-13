ae1922b96e694f3336a99b9b0a7fd647d02f94c7
Sharbi <seanharbison001@gmail.com>
2019-02-13 18:20:52 +0000

    changed discriminator kernel shapes

--- ssgan.py
+++ ssgan.py
@@ -189,7 +189,7 @@ class Discriminator(nn.Module):
             nn.Dropout(0.2),
 
             # input is (number_channels) x 60 x 4
-            nn.utils.weight_norm(nn.Conv2d(nc, ndf, (1, 2), padding=1, bias=False)),
+            nn.utils.weight_norm(nn.Conv2d(nc, ndf, 3, padding=1, bias=False)),
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.Conv2d(ndf, ndf, (1, 2), padding=1, bias=False)),
             nn.LeakyReLU(0.2),
@@ -197,7 +197,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
             # (ndf) x 30 x 2
-            nn.utils.weight_norm(nn.Conv2d(ndf, ndf *2, (1, 2), padding=1, bias=False)),
+            nn.utils.weight_norm(nn.Conv2d(ndf, ndf *2, 3, padding=1, bias=False)),
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, (1, 2), padding=1, bias=False)),
             nn.LeakyReLU(0.2),
@@ -205,7 +205,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
             # (ndf) x 15 x 1
-            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, (1, 2), padding=0, bias=False)),
+            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, 1, padding=0, bias=False)),
             nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
         )
