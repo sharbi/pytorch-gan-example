778c50b179edc392cd7293316fca764941a22fec
Sharbi <seanharbison001@gmail.com>
2019-02-13 18:05:34 +0000

    Syntax fixes

--- ssgan.py
+++ ssgan.py
@@ -236,13 +236,6 @@ class Discriminator(nn.Module):
         return out, class_logits, gan_logits, features
 
 
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
-        nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
 
 
 def one_hot(labels):
