4fd396f05ed4ff7d92a9e86862bc7f70a065bc54
Sharbi <seanharbison001@gmail.com>
2019-02-13 18:26:20 +0000

    changed discriminator kernel shapes

--- ssgan.py
+++ ssgan.py
@@ -199,7 +199,7 @@ class Discriminator(nn.Module):
             # (ndf) x 30 x 2
             nn.utils.weight_norm(nn.Conv2d(ndf, ndf *2, 3, padding=1, bias=False)),
             nn.LeakyReLU(0.2),
-            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, 2, padding=1, bias=False)),
+            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, (1, 2), padding=1, bias=False)),
             nn.LeakyReLU(0.2),
             nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*2, 3, padding=1, stride=2, bias=False)),
             nn.LeakyReLU(0.2),
