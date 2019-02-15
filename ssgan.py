5f11ae88beff6d92969e944d846ca7cf15362e91
Sharbi <seanharbison001@gmail.com>
2019-02-15 09:38:01 +0000

    syntax fixes

--- ssgan.py
+++ ssgan.py
@@ -69,7 +69,7 @@ class DiabetesDataset(Dataset):
         data = data[1:6]
 
 
-        data = np.expand_dims(data, 0)
+        data = np.expand_dims(data, 1)
         data = np.expand_dims(data, 0)
 
         if self._is_train_dataset():
