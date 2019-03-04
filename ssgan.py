eba3a7b1b7367a40c2517d6db306aee0e0efde85
Sharbi <seanharbison001@gmail.com>
2019-03-04 10:10:44 +0000

    Added layers to generator

--- ssgan.py
+++ ssgan.py
@@ -136,7 +136,7 @@ class Generator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # Input is Z, going into convolution
-            nn.ConvTranspose2d(ngf*8, ngf * 8, 2, (1, 2), 0, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 8, 2, (1, 2), 0, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             nn.BatchNorm2d(ngf * 8),
             nn.Dropout(0.2),
