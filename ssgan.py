35fafd670af74760c3eb0088edf5476c6a50e2c6
Sharbi <seanharbison001@gmail.com>
2019-03-08 06:40:50 +0000

    Reduced expanded dims in dataloader

--- ssgan.py
+++ ssgan.py
@@ -66,7 +66,6 @@ class DiabetesDataset(Dataset):
         data = data[1:6]
 
         data = np.expand_dims(data, axis=0)
-        data = np.expand_dims(data, axis=0)
 
 
         if self._is_train_dataset():
