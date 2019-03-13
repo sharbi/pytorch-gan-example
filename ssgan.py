f471f782499657ab19a90ab674eeb58d0090b692
Sharbi <seanharbison001@gmail.com>
2019-03-13 14:01:34 +0000

    Fixed extended labels

--- ssgan.py
+++ ssgan.py
@@ -273,7 +273,7 @@ for epoch in range(num_epochs):
         unlabeled_data = _to_var(unlabeled_data).float()
         labels = _to_var(labels).long().squeeze()
 
-        extended_labels = torch.cat((labels, torch.zeros_like(labels.data.shape[0]).long()), 1)
+        extended_labels = torch.cat((labels, torch.zeros(labels.data.shape[0]).long()), 1)
 
 
         noise = torch.FloatTensor(batch_size, nz, 1, 1)
