4127cc7163d5a52d6da2516db859cab265ecba0a
Sharbi <seanharbison001@gmail.com>
2019-03-16 03:01:35 +0000

    testing feature loss

--- ssgan.py
+++ ssgan.py
@@ -195,7 +195,6 @@ class Discriminator(nn.Module):
             # (ndf) x 15 x 1
             nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*4, 1, padding=0, bias=False)),
             nn.LeakyReLU(0.2),
-            nn.Dropout(0.5)
         )
 
         self.features = nn.AvgPool2d(kernel_size=2)
