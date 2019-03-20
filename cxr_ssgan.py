5d5ba9010a30803ad54fa4e308d757a07c4b095c
Sharbi <seanharbison001@gmail.com>
2019-03-20 16:52:51 +0000

    Change Disc shape

--- cxr_ssgan.py
+++ cxr_ssgan.py
@@ -249,7 +249,7 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
 
-            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*4, 4, 1, 1, bias=False)),
+            nn.utils.weight_norm(nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False)),
             nn.LeakyReLU(0.2),
             nn.Dropout(0.5),
 
