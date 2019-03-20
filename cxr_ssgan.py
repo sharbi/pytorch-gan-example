bb4a7e693b0a92f10b24b1ea1f72bc7ad317114d
Sharbi <seanharbison001@gmail.com>
2019-03-20 17:16:25 +0000

    Changed timing of one hot labeling

--- cxr_ssgan.py
+++ cxr_ssgan.py
@@ -89,6 +89,7 @@ class CXRDataset(Dataset):
         img_name = os.path.join(self.root_dir, self.info.iloc[idx, 1])
         image = Image.open(img_name)
         labels = self.one_hot_labels[idx]
+        print(labels)
 
         age = self.info.iloc[idx, 5]
         gender = self.info.iloc[idx, 6]
