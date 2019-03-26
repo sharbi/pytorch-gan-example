ffd716a8fc215bad2d5b00ac9005f1b80c656a67
Sharbi <seanharbison001@gmail.com>
2019-03-26 23:00:40 +0000

    Changed label loading within training

--- cxr_ssgan.py
+++ cxr_ssgan.py
@@ -446,9 +446,8 @@ for epoch in range(num_epochs):
 
         thresholder_predictions = torch.sigmoid(logits_lab)
         preds = map(apply_threshold, thresholder_predictions)
-        print(list(preds))
         total = len(labels) * num_classes
-        correct = (list(preds) == labels.cpu().numpy().astype(int)).sum()
+        correct = (list(preds) == labels.cpu().cpu().numpy().astype(int)).sum()
         train_accuracy = 100 * correct / total
 
         if i % 50 == 0:
