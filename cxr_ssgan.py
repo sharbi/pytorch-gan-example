eb92751aaa9f8f5066f8f8b5d1c666f1c5115fd9
Sharbi <seanharbison001@gmail.com>
2019-03-27 14:33:47 +0000

    Testing flattened feature matching. Also changed loss for supervised to try include label masking

--- cxr_ssgan.py
+++ cxr_ssgan.py
@@ -343,7 +343,7 @@ for epoch in range(num_epochs):
 
         #loss_lab = d_gan_criterion(logits_lab, labels)
 
-        loss_lab = -torch.sum(labels * torch.log(torch.sigmoid(logits_lab)), dim=1)
+        loss_lab = -torch.sum(labels * torch.log(F.softplus(logits_lab)), dim=1)
         loss_lab = (loss_lab * mask) / torch.max(_to_var(torch.Tensor([(1.0), torch.sum(mask)])))
         loss_lab = torch.mean(loss_lab)
 
@@ -385,6 +385,9 @@ for epoch in range(num_epochs):
         loss_unl = - 0.5 * torch.mean(l_unl) \
                          + 0.5 * torch.mean(F.softplus(l_unl)) \
                          + 0.5 * torch.mean(F.softplus(l_gen))
+
+        print(real_real)
+        print(fake_fake)
 
         #manifold_diff = logits_gen - logits_gen_adv
 
